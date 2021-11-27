dbf829c53d4b9fc32f1a7760869a37c2f3ae74ba
AllenBoxinHao <allen7hao@gmail.com>
2021-11-27 19:13:36 +1100

    eslint config update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.scss';
-import Home from './pages/Home'
+import Home from './pages/Home';
 import Header from './app/Header';
 import Footer from './app/Footer';
-console.log(123);
 
 const App = () => (
   <>
-  <Header/>
-  <Home/>
-  <Footer/>
+    <Header />
+    <Home />
+    <Footer />
   </>
-
 );
-
 
 export default App;
