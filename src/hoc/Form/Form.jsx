aa2fd18054bcaeb00cfdb96af627625357f77878
IRISLI0224 <38321032+IRISLI0224@users.noreply.github.com>
2021-12-05 16:41:52 +1100

    add Form in HOC

---
+++ src/hoc/Form/Form.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Item = styled.div`
+    max-height: 100px;
+    overflow: auto;
+    display: flex;
+    align-items: flex-start;
+    width: 100%;
+    padding-top: 0.5rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
+`;
+
+const Label = styled.label`
+  margin: 0 0 5px 5px;
+`;
+
+const Form = ({label,htmlFor,children}) => (
+  <Item>
+    {label && <Label htmlFor={htmlFor}>{label}</Label>}
+    {children}
+  </Item>
+ );
+
+ Form.propTypes = {
+  label: PropTypes.string.isRequired,
+  htmlFor: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+
+export default Form;
