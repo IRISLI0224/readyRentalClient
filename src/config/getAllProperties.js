22257f2ca50201616610b8c13a8c9fcfab45d2bd
IRISLI0224 <38321032+IRISLI0224@users.noreply.github.com>
2021-12-11 11:46:59 +1100

    fix spacing

--- src/config/getAllProperties.js
+++ src/config/getAllProperties.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
  const API_GET_ALL_PROPERTIES = '/properties';
 
-
-
-
 const getAllProperties = async () => {
     const url = `http://localhost:8080/api/v1`+API_GET_ALL_PROPERTIES;
     const response = await axios.get(url);
