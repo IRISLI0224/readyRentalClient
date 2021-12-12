80d5ed032111f687bdff582339c373444c311373
IRISLI0224 <38321032+IRISLI0224@users.noreply.github.com>
2021-12-12 11:48:42 +1100

    remove console

--- src/pages/PropertyDetails/PropertyDetails.jsx
+++ src/pages/PropertyDetails/PropertyDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import BasicInfo from './components/BasicInfo';
 import Description from './components/Description/Description';
-import {getPropertiesById} from '../../config/Properties'
+import {getPropertiesById} from '../../config/properties'
 
 const PropertyDetails = () => {
   const params = useParams();
