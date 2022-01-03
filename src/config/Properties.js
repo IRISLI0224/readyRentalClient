c8ff504cec81980b075e996fd4fd0b0fce55664a
Jessie Liu <hanyan.liu@outlook.com>
2022-01-03 15:34:41 +1100

    property delete function finished

--- src/config/Properties.js
+++ src/config/Properties.js
@@ -6,14 +6,15 @@ import { getToken } from '../utils/authentication';
 const devURL = 'http://localhost:8080/api/v1';
 
 //Get all property
+//todo: need to delete one
 const API_GET_ALL_PROPERTIES = '/properties';
+const API_GET_PROPERTIES_BY_ID = '/properties/';
 
 export const getAllProperties = async () => {
   const url = devURL + API_GET_ALL_PROPERTIES;
   const response = await axios.get(url);
   return response.data;
 };
-const API_GET_PROPERTIES_BY_ID = '/properties/';
 
 export const getPropertiesById = async (Id) => {
   const url = devURL + API_GET_PROPERTIES_BY_ID + Id;
@@ -26,16 +27,28 @@ const API_POST_PROPERTY = '/properties';
 
 export const PostProperty = async (propertyInfo) => {
   const json = JSON.stringify(propertyInfo);
-  const token =getToken();
+  const token = getToken();
   const config = {
-    headers: {  'Content-Type': 'application/json', Authorization: `Bearer ${token}` }
-};
-  const res =axios.post(`${devURL}`+API_POST_PROPERTY, json, config)
-  return res 
+    headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
+  };
+  const res = axios.post(`${devURL}` + API_POST_PROPERTY, json, config);
+  return res;
 };
 
 export const getPropertiesBySearch = async (search) => {
   const url = `http://localhost:8080/api/v1/properties${search}`;
   const response = await axios.get(url);
+  return response.data;
+};
+
+//Delete property by id
+export const deletePropertyById = async (propertyId) => {
+  const token = getToken();
+  const config = {
+    headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
+  };
+  const url = `${devURL}${API_GET_ALL_PROPERTIES}/${propertyId}`;
+  const response = await axios.delete(url, config);
+  console.log(response.data);
   return response.data;
 };
--- src/pages/ListedProperties/component/Wrapper/Wrapper.jsx
+++ src/pages/ListedProperties/component/Wrapper/Wrapper.jsx
@@ -12,6 +12,18 @@ export const WrapperColumnFlex = styled(Wrapper)`
   flex-direction: column;
   box-shadow: none;
 `;
+
+export const PopUpContainer = styled(Wrapper)`
+  flex-direction: column;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0.2rem 0.2rem 0px;
+  width: 50%;
+`;
+
 export const NoShadowWrapper = styled(Wrapper)`
   box-shadow: none;
+  justify-content: space-between;
+`;
+
+export const NoShadowWrapperSpaceAround = styled(NoShadowWrapper)`
+  justify-content: space-around;
 `;
