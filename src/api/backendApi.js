76166ce713aa9695ed5ca541d7238aa177cc6270
Jessie Liu <hanyan.liu@outlook.com>
2022-01-15 23:15:50 +1100

    delete extra testing code

--- src/api/backendApi.js
+++ src/api/backendApi.js
@@ -1,15 +1,12 @@
 import axios from 'axios';
 
-// const baseURL =
-//   process.env.NODE_ENV === 'development'
-//     ? 'http://localhost:8080/api/v1'
-//     : 'http://3.26.189.227:8000/api/v1';
-
-// const localURL = 'http://localhost:8080/api/v1';
-const productionURL = 'http://3.26.189.227:8000/api/v1';
+const baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080/api/v1'
+    : 'http://3.26.189.227:8000/api/v1';
 
 const backendApi = axios.create({
-  baseURL: productionURL,
+  baseURL: baseURL,
 });
 
 export default backendApi;
